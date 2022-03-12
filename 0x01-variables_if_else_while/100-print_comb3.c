7837e2688a57dbb2e1de8fb6c601334a6b0f0580
CliffordKasera <ckasera6486@gmail.com>
2022-03-12 02:23:18 +0000

    remove trailing white spaces

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -5,7 +5,6 @@
  *Description: print double digit combos
  *Return: Always 0 (Success)
  **/
-
 int main(void)
 {
 	int i, j;
@@ -20,17 +19,15 @@ int main(void)
 		{
 			putchar(i);
 			putchar(j);
-
-																											if (i < 56 || j < 57)
-																											{
-																												putchar(44);
-																												putchar(32);
-																											}
-																											j++;
-																											}
+			if (i < 56 || j < 57)
+			{
+				putchar(44);
+				putchar(32);
+			}
+			j++;
+		}
 		i++;
 	}
-
 	putchar(10);
 
 	return (0);
