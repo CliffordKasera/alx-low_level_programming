680451044f5ad5952e2399c1e109f3484cfb376b
CliffordKasera <ckasera6486@gmail.com>
2022-04-21 23:12:21 +0000

    fix error in logic

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -10,35 +10,28 @@
  */
 list_t *add_node_end(list_t **head, const char *str)
 {
-	list_t *current;
-	list_t *new_node;
-	int c;
+	list_t *new, *current;
+	char *s;
 
+	if (str == NULL)
+		return (NULL);
+	s = strdup(str);
+	if (s == NULL)
+		return (NULL);
+	new = malloc(sizeof(list_t));
+	if (new == NULL)
+		return (NULL);
+	new->str = s;
+	new->len = strlen(str);
+	new->next = NULL;
+	if (*head == NULL)
+	{
+		*head = new;
+		return (*head);
+	}
 	current = *head;
-	while (current && current->next != NULL)
+	while (current->next != NULL)
 		current = current->next;
-
-	for (c = 0; str[c] != '\0'; c++)
-		;
-
-	new_node = malloc(sizeof(list_t));
-	if (new_node == NULL)
-	{
-		free(new_node);
-		return (NULL);
-	}
-	new_node->str = strdup(str);
-	if (new_node->str == NULL)
-	{
-		free(new_node);
-		return (NULL);
-	}
-	new_node->len = c;
-	new_node->next = NULL;
-
-	if (current)
-		current->next = new_node;
-	else
-		*head = new_node;
-	return (new_node);
+	current->next = new;
+	return (*head);
 }
