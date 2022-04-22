33a32ed74e1624693ffc43a26d020c5d09107d71
CliffordKasera <ckasera6486@gmail.com>
2022-04-22 00:14:21 +0000

    fix bugs

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -1,4 +1,5 @@
 #include "lists.h"
+#include <stdlib.h>
 
 /**
  * free_list - frees a list_t list
@@ -6,16 +7,13 @@
  */
 void free_list(list_t *head)
 {
-	list_t *current;
-	list_t *nxt;
-
-	current = head;
-
-	while (current != NULL)
+	if (head)
 	{
-		nxt = current->next;
-		free(current->str);
-		free(current);
-		current = nxt;
+		if (head->next)
+			free_list(head->next);
+		if (head->str)
+			free(head->str);
+		if (head)
+			free(head);
 	}
 }
